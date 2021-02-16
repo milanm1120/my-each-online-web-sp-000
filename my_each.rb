23c1f366547cd701b9ddd46b3435c4ee778204bd
Milan Mody <milanm1120@gmail.com>
2021-02-16 00:38:37 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  return_values = []
+  while i < array.length
+    return_values << yield(array[i])
+    i += 1
+  end
+
+  if return_values.include?(false)
+    false
+  else
+    true
+  end
+    array
 end
