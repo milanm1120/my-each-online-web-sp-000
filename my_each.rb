390e0c55e738df6d7555bbfd6c2b15c508750d87
Milan Mody <milanm1120@gmail.com>
2021-02-22 23:46:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
 def my_each(array)
+  if block_given?
+    new_array = []
+    i = 0
+    while i < array.length
+      new_array<<yield(array[i])
+    end
+    return array
+end
 
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
